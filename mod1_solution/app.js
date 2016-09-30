8ac1d5734a7c7bd15604eff174a117e226d34cb5
wavegate <cc.frankee@gmail.com>
2016-09-30 13:14:33 -0500

    update solution

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -7,17 +7,15 @@
     $scope.message = "";
 
     $scope.checkMenu = function () {
-      var message = document.getElementById('message');
-      var textBox = document.getElementById('lunch-menu');
       if ($scope.lunchMenu) {
-        message.style.color = "green";
-        textBox.style.border = "1px solid green";
+        $scope.messageStyle = {color: "green"};
+        $scope.menuStyle = {border: "1px solid green"};
         var numItems = $scope.lunchMenu.split(',').
           filter(function (entry) { return entry.trim() != '';}).length;
         $scope.message = (numItems <= 3) ? "Enjoy!" : "Too much!";
       } else {
-        message.style.color = "red";
-        textBox.style.border = "1px solid red";
+        $scope.messageStyle = {color: "red"};
+        $scope.menuStyle = {border: "1px solid red"};
         $scope.message = "Please enter data first"
       }
     }
